d9f7169b66ac8ac583b5572843a5650163a3d0cf
muhammad ruhiyat <muhammadruhiyat020998@gmail.com>
2022-09-12 10:00:35 +0700

    membuat DB seed untuk tabel ref_kelompok_sk

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,7 +3,11 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use DateTime;
+use Faker\Core\DateTime as CoreDateTime;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class DatabaseSeeder extends Seeder
 {
@@ -19,6 +23,42 @@ class DatabaseSeeder extends Seeder
         // \App\Models\User::factory()->create([
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
+        // ]);
+
+        // DB::table('ref_kelompok_sk')->insert([
+        //     'kelompok' => 'Finansial',
+        //     'created_at' => date('Ymd'),
+        //     'updated_at' => date('Ymd'),
+        //     'deleted_at' => date('Ymd')
+
+        // ]);
+
+        // DB::table('ref_kelompok_sk')->insert([
+        //     'kelompok' => 'Pelanggan',
+        //     'created_at' => date('Ymd'),
+        //     'updated_at' => date('Ymd'),
+        //     'deleted_at' => date('Ymd')
+        // ]);
+
+        // DB::table('ref_kelompok_sk')->insert([
+        //     'kelompok' => 'Proses Internal',
+        //     'created_at' => date('Ymd'),
+        //     'updated_at' => date('Ymd'),
+        //     'deleted_at' => date('Ymd')
+        // ]);
+
+        // DB::table('ref_kelompok_sk')->insert([
+        //     'kelompok' => 'Pertumbuhan & Pembelajaran',
+        //     'created_at' => date('Ymd'),
+        //     'updated_at' => date('Ymd'),
+        //     'deleted_at' => date('Ymd')
+        // ]);
+
+        // DB::table('ref_kelompok_sk')->insert([
+        //     'kelompok' => 'Lain - lain',
+        //     'created_at' => date('Ymd'),
+        //     'updated_at' => date('Ymd'),
+        //     'deleted_at' => date('Ymd')
         // ]);
     }
 }
