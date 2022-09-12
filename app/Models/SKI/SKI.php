896372b2b5f34c59be8f86836487598d7a379c29
muhammad ruhiyat <muhammadruhiyat020998@gmail.com>
2022-09-12 09:58:35 +0700

    mebuat model SKI

---
+++ app/Models/SKI/SKI.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class SKI extends Model
+{
+    use HasFactory;
+
+    public function index()
+    {
+        # code...
+    }
+
+    public function ski_SK()
+    {
+        # code...
+    }
+}
