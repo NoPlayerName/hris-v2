0db2738222340a8176d9374dae6f1c4b33cc3cb9
muhammad ruhiyat <muhammadruhiyat020998@gmail.com>
2022-09-13 12:58:05 +0700

    menambahkan controller SKISk

---
+++ app/Http/Controllers/SKISkController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\SKISk;
+use App\Http\Requests\StoreSKISkRequest;
+use App\Http\Requests\UpdateSKISkRequest;
+
+class SKISkController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \App\Http\Requests\StoreSKISkRequest  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(StoreSKISkRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\SKISk  $sKISk
+     * @return \Illuminate\Http\Response
+     */
+    public function show(SKISk $sKISk)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\SKISk  $sKISk
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(SKISk $sKISk)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \App\Http\Requests\UpdateSKISkRequest  $request
+     * @param  \App\Models\SKISk  $sKISk
+     * @return \Illuminate\Http\Response
+     */
+    public function update(UpdateSKISkRequest $request, SKISk $sKISk)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\SKISk  $sKISk
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(SKISk $sKISk)
+    {
+        //
+    }
+}
