f6d7a93d0dceb7499034c4c4d04b61d5b5bc2b81
muhammad ruhiyat <muhammadruhiyat020998@gmail.com>
2022-09-13 10:58:48 +0700

    membuat controller, model , factory, seed ski_tugas

---
+++ database/factories/SkiTugasFactory.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Ski_tugas>
+ */
+class SkiTugasFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            //
+        ];
+    }
+}
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,9 +5,10 @@ namespace Database\Seeders;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 
 use DateTime;
-use Faker\Core\DateTime as CoreDateTime;
+use App\Models\Ski;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
+use Faker\Core\DateTime as CoreDateTime;
 
 class DatabaseSeeder extends Seeder
 {
@@ -18,47 +19,60 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+        // Ski::factory(5)->create();
         // \App\Models\User::factory(10)->create();
 
         // \App\Models\User::factory()->create([
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+        DB::table('skis')->insert([
+            'category' => 'data',
+            'created_at' => date('Ymd'),
+            'updated_at' => date('Ymd'),
+            'deleted_at' => date('Ymd')
+        ]);
+        DB::table('skis')->insert([
+            'category' => 'data2',
+            'created_at' => date('Ymd'),
+            'updated_at' => date('Ymd'),
+            'deleted_at' => date('Ymd')
+        ]);
 
-        // DB::table('ref_kelompok_sk')->insert([
-        //     'kelompok' => 'Finansial',
-        //     'created_at' => date('Ymd'),
-        //     'updated_at' => date('Ymd'),
-        //     'deleted_at' => date('Ymd')
+        DB::table('kelompok_sks')->insert([
+            'kelompok' => 'Finansial',
+            'created_at' => date('Ymd'),
+            'updated_at' => date('Ymd'),
+            'deleted_at' => date('Ymd')
 
-        // ]);
+        ]);
 
-        // DB::table('ref_kelompok_sk')->insert([
-        //     'kelompok' => 'Pelanggan',
-        //     'created_at' => date('Ymd'),
-        //     'updated_at' => date('Ymd'),
-        //     'deleted_at' => date('Ymd')
-        // ]);
+        DB::table('kelompok_sks')->insert([
+            'kelompok' => 'Pelanggan',
+            'created_at' => date('Ymd'),
+            'updated_at' => date('Ymd'),
+            'deleted_at' => date('Ymd')
+        ]);
 
-        // DB::table('ref_kelompok_sk')->insert([
-        //     'kelompok' => 'Proses Internal',
-        //     'created_at' => date('Ymd'),
-        //     'updated_at' => date('Ymd'),
-        //     'deleted_at' => date('Ymd')
-        // ]);
+        DB::table('kelompok_sks')->insert([
+            'kelompok' => 'Proses Internal',
+            'created_at' => date('Ymd'),
+            'updated_at' => date('Ymd'),
+            'deleted_at' => date('Ymd')
+        ]);
 
-        // DB::table('ref_kelompok_sk')->insert([
-        //     'kelompok' => 'Pertumbuhan & Pembelajaran',
-        //     'created_at' => date('Ymd'),
-        //     'updated_at' => date('Ymd'),
-        //     'deleted_at' => date('Ymd')
-        // ]);
+        DB::table('kelompok_sks')->insert([
+            'kelompok' => 'Pertumbuhan & Pembelajaran',
+            'created_at' => date('Ymd'),
+            'updated_at' => date('Ymd'),
+            'deleted_at' => date('Ymd')
+        ]);
 
-        // DB::table('ref_kelompok_sk')->insert([
-        //     'kelompok' => 'Lain - lain',
-        //     'created_at' => date('Ymd'),
-        //     'updated_at' => date('Ymd'),
-        //     'deleted_at' => date('Ymd')
-        // ]);
+        DB::table('kelompok_sks')->insert([
+            'kelompok' => 'Lain - lain',
+            'created_at' => date('Ymd'),
+            'updated_at' => date('Ymd'),
+            'deleted_at' => date('Ymd')
+        ]);
     }
 }
