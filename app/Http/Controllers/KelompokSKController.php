69e41e19c7f5623dd0c741d436d1c12dbf4eb11e
muhammad ruhiyat <muhammadruhiyat020998@gmail.com>
2022-09-13 12:36:59 +0700

    menambahkan controller KelompokSK

---
+++ app/Http/Controllers/KelompokSKController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\KelompokSK;
+use App\Http\Requests\StoreKelompokSKRequest;
+use App\Http\Requests\UpdateKelompokSKRequest;
+
+class KelompokSKController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \App\Http\Requests\StoreKelompokSKRequest  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(StoreKelompokSKRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\KelompokSK  $kelompokSK
+     * @return \Illuminate\Http\Response
+     */
+    public function show(KelompokSK $kelompokSK)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\KelompokSK  $kelompokSK
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(KelompokSK $kelompokSK)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \App\Http\Requests\UpdateKelompokSKRequest  $request
+     * @param  \App\Models\KelompokSK  $kelompokSK
+     * @return \Illuminate\Http\Response
+     */
+    public function update(UpdateKelompokSKRequest $request, KelompokSK $kelompokSK)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\KelompokSK  $kelompokSK
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(KelompokSK $kelompokSK)
+    {
+        //
+    }
+}
