d2fa856eb68770a495b5ba7a7785dc4159b66856
muhammad ruhiyat <muhammadruhiyat020998@gmail.com>
2022-09-13 12:50:02 +0700

    menambahkan controler SKITugas

---
+++ app/Http/Controllers/SKITugasController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\SKITugas;
+use App\Http\Requests\StoreSKITugasRequest;
+use App\Http\Requests\UpdateSKITugasRequest;
+
+class SKITugasController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \App\Http\Requests\StoreSKITugasRequest  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(StoreSKITugasRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\SKITugas  $sKITugas
+     * @return \Illuminate\Http\Response
+     */
+    public function show(SKITugas $sKITugas)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\SKITugas  $sKITugas
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(SKITugas $sKITugas)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \App\Http\Requests\UpdateSKITugasRequest  $request
+     * @param  \App\Models\SKITugas  $sKITugas
+     * @return \Illuminate\Http\Response
+     */
+    public function update(UpdateSKITugasRequest $request, SKITugas $sKITugas)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\SKITugas  $sKITugas
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(SKITugas $sKITugas)
+    {
+        //
+    }
+}
