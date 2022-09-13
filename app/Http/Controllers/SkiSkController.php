c65fdc7d1bdaa4a02e3981b64146054d691e6e87
muhammad ruhiyat <muhammadruhiyat020998@gmail.com>
2022-09-13 11:13:27 +0700

    membuat model, controller, factory, seed ski_sk

---
+++ app/Http/Controllers/SkiSkController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Ski_sk;
+use App\Http\Requests\StoreSki_skRequest;
+use App\Http\Requests\UpdateSki_skRequest;
+
+class SkiSkController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \App\Http\Requests\StoreSki_skRequest  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(StoreSki_skRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Ski_sk  $ski_sk
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Ski_sk $ski_sk)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Ski_sk  $ski_sk
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Ski_sk $ski_sk)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \App\Http\Requests\UpdateSki_skRequest  $request
+     * @param  \App\Models\Ski_sk  $ski_sk
+     * @return \Illuminate\Http\Response
+     */
+    public function update(UpdateSki_skRequest $request, Ski_sk $ski_sk)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Ski_sk  $ski_sk
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Ski_sk $ski_sk)
+    {
+        //
+    }
+}
