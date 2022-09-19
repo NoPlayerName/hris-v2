55ea7fc291a7e9f31810e8ecdd9a85f012b7e018
muhammad ruhiyat <muhammadruhiyat020998@gmail.com>
2022-09-19 14:16:09 +0700

    menambahkan migration

---
+++ database/migrations/2022_09_19_051721_create_ski_sks_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class () extends Migration {
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('ski_sks', function (Blueprint $table) {
+            $table->id('id_ref_ski_sk');
+            $table->foreignId('id_ref_ski');
+            $table->foreignId('id_kelompok_sk');
+            $table->text('sasaran_kerja');
+            $table->float('bobot');
+            $table->text('kriteria_pengukuran');
+            $table->text('target');
+            $table->datetime('batas_waktu');
+            $table->text('rencana_tindakan');
+            $table->integer('sequence');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('ski_sks');
+    }
+};
