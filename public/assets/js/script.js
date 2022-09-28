cefe141e3200fe62284eef7e476bee08f985734e
muhammad ruhiyat <muhammadruhiyat020998@gmail.com>
2022-09-28 13:30:25 +0700

    script js custom

---
+++ public/assets/js/script.js
@@ -0,0 +1,11 @@
+$(function () {
+    //addtugas
+    $(".addtugas").on("click", function () {
+        $("#ModalLabel").html("Add Tugas");
+    });
+
+    //addski
+    $(".addski").on("click", function () {
+        $("#ModalLabel").html("Add Sasaran Kerja Individu");
+    });
+});
