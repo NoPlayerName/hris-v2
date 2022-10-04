42357c39f5bc2d6fe451fb5aa3dd0552e97386b4
muhammad ruhiyat <muhammadruhiyat020998@gmail.com>
2022-10-04 11:07:14 +0700

    menambahkan seed tabel ski_sk

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -26,17 +26,59 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
-        DB::table('skis')->insert([
-            'category' => 'data',
+
+        DB::table('ski_sks')->insert([
+        'id_ref_ski' => 1,
+        'id_kelompok_sk' => 1,
+        'sasaran_kerja' => 'kinerja tahun 2021',
+        'bobot' => 0.3,
+        'kriteria_pengukuran' => 'yang memenuhi kriteri adalah semua target yang diberikan tercapai',
+        'target' => 'semua target terpenuhi',
+        'batas_waktu' => date('Ymd'),
+        'rencana_tindakan' => 'mencapai semua target',
+        'sequence' => 1
+        ]);
+        DB::table('ski_sks')->insert([
+        'id_ref_ski' => 2,
+        'id_kelompok_sk' => 2,
+        'sasaran_kerja' => 'kinerja tahun 2022',
+        'bobot' => 0.2,
+        'kriteria_pengukuran' => 'yang memenuhi kriteri adalah hampir semua target yang diberikan tercapai',
+        'target' => 'sebagian target terpenuhi',
+        'batas_waktu' => date('Ymd'),
+        'rencana_tindakan' => 'mencapai sebagiantarget',
+        'sequence' => 2
+        ]);
+        DB::table('ski_sks')->insert([
+        'id_ref_ski' => 3,
+        'id_kelompok_sk' => 3,
+        'sasaran_kerja' => 'kinerja tahun 2023',
+        'bobot' => 0.4,
+        'kriteria_pengukuran' => 'target tercapai semua',
+        'target' => 'target terlampaui',
+        'batas_waktu' => date('Ymd'),
+        'rencana_tindakan' => 'target tercapai',
+        'sequence' => 3
+        ]);
+        DB::table('ski_templates')->insert([
+            'category' => 'staff',
             'kode_jabatan' => '01',
             'version' => date('Ymd'),
             // 'created_at' => date('Ymd'),
             // 'updated_at' => date('Ymd'),
             // 'deleted_at' => date('Ymd')
         ]);
-        DB::table('skis')->insert([
-            'category' => 'data2',
+        DB::table('ski_templates')->insert([
+            'category' => 'supervisor',
             'kode_jabatan' => '02',
+            'version' => date('Ymd'),
+            // 'created_at' => date('Ymd'),
+            // 'updated_at' => date('Ymd'),
+            // 'deleted_at' => date('Ymd')
+        ]);
+        DB::table('ski_templates')->insert([
+            'category' => 'Section Head',
+            'kode_jabatan' => '03',
             'version' => date('Ymd'),
             // 'created_at' => date('Ymd'),
             // 'updated_at' => date('Ymd'),
